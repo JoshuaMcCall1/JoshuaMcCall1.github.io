c0dfa6774ef165f2abed34d6ae7d62318cbe5634
JoshuaMcCall1 <45631405+JoshuaMcCall1@users.noreply.github.com>
2018-12-13 11:24:29 -0600

    Add files via upload

--- dicegame.js
+++ dicegame.js
@@ -1,1 +1,73 @@
+// let iRandomNumber;
+// let iRollDice;
+// let iroll;
+// {
+
+// iRollDice =prompt ("how many times want to roll the dice,1-5")}
+// let iRandomNumber=iRollDice.iroll("");
+// for (let i = 1;  i<iRollDice; i++)
+
+// Math.floor(Math.random() * numberOfSides) + 1;
+
+
+// console.log()
+
+
+
+
+function rolldice(numberOfSides){
+	let rusult = Math.floor((Math.random() *numberOfSides) +1);
+
+}
+
+
+
+function firstOne (){
+	let firstRoll = 0
+
+for (let i = 0; i <6; i++) {
+firstRoll += rolldice (6);
+}
+}
+
+
+console.log(rolldice(6));
+
+
+function secondOne (){
+		let secondOne=0
+for (let i = 0; i <5; i++) {
+
+}
+}
+
+console.log(rolldice(5));
+
+
+
+function thirdOne (){
+		let thirdOne=0
+for (let i = 0; i <4; i++) {
+
+
+}
+}
+{
+console.log(rolldice(4));
+
+
+
+function fourthOne(){
+		let fourthOne=0
+for (let  i = 0; i <3; i++) {
+
+}
+}
+
+console.log(rolldice(3));
+
+
+
+
+
 
